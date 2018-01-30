2864fb9108cc1c5643f30a0f445516f232c656d6
Andy Maskiell <maskiella@gmail.com>
2018-01-30 21:56:05 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,13 +6,12 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
 end
 
-
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS species_count FROM characters GROUP BY characters.species ORDER BY species_count DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors JOIN subgenres ON authors.id = subgenres.id"
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
